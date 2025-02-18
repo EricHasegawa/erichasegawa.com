b130284ef56faef3cd9d2f73dc6eb3612fb1e8e1
Eric Hasegawa <erichasegawa1@gmail.com>
2025-02-17 21:18:45 -0800

    feat: create table for blog posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_17_000705) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_18_050521) do
+  create_table "Post", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "sessions", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "ip_address"
