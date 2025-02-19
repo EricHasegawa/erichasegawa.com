4d515c5eb06574b6cba871ea9af474d363402d52
Eric Hasegawa <erichasegawa1@gmail.com>
2025-02-18 18:40:45 -0800

    feat: add user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resource :session
   resources :passwords, param: :token
+  resources :registrations, only: [:new, :create]
+
   root "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
