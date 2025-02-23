d590fb95bf8a9d444c35770e628578dc7be65560
Eric Hasegawa <erichasegawa1@gmail.com>
2025-02-22 18:08:20 -0800

    fix: make linting pass

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resource :session
   resources :passwords, param: :token
-  resources :registrations, only: [:new, :create]
+  resources :registrations, only: [ :new, :create ]
 
   root "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
