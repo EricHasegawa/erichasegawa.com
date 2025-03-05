6a2b861377f74124dd51e8b110e73fc33436bb58
Eric Hasegawa <erichasegawa1@gmail.com>
2025-03-04 17:24:48 -0800

    chore: disable registration

--- test/controllers/registrations_controller_test.rb
+++ test/controllers/registrations_controller_test.rb
@@ -7,17 +7,21 @@ class RegistrationsControllerTest < ActionDispatch::IntegrationTest
     assert_select "form"
   end
 
-  test "should create user with valid parameters" do
-    assert_difference("User.count", 1) do
+  test "should not create user with valid parameters" do
+    assert_no_difference("User.count") do
       post registrations_url, params: { user: { email_address: "test@example.com", password: "password123", password_confirmation: "password123" } }
     end
-    assert_redirected_to root_url
+    assert_equal new_registration_url, response.location
+    assert_equal "Sign-ups are currently disabled.", flash[:alert]
+    assert_response :forbidden
   end
 
   test "should not create user with invalid parameters" do
     assert_no_difference("User.count") do
       post registrations_url, params: { user: { email_address: "", password: "password123", password_confirmation: "password123" } }
     end
-    assert_response :unprocessable_entity
+    assert_equal new_registration_url, response.location
+    assert_equal "Sign-ups are currently disabled.", flash[:alert]
+    assert_response :forbidden
   end
 end
